fd8432aa360f7444c5b8c8fcb48d03fb7943aff6
luisinn <162268461+TheLuisinn@users.noreply.github.com>
2024-06-22 00:48:19 -0300

    function.py

--- function.py
+++ function.py
@@ -1,4 +1,4 @@
-RED = "\033[1;31m"
+RED = "\033[1;31m" # Definição de Cores para uso no codigo
 PINK = "\033[1;35m"
 GREEN = "\033[1;32m"
 RESET = "\033[0m"
@@ -34,9 +34,10 @@ def cadastrar_dados():
             medicação_continua= input("Insira alguma medica de uso continua do paciente: ")
             observações = input("Insira alguma observação do paciente: ")
 
+            # Formatar os dados do cliente e paciente em uma única string
             cliente = f"{codigo_cliente},{contato},{endereco},{bairro},{cep},{ponto_de_referencia},{animal},{nome_paciente},{raça},{cor_da_pelagem},{peso},{data_de_nascimento},{restrição_alimentar},{medicação_continua},{observações}\n" # Formatar os dados do cliente em uma string
 
-            arquivo.write(cliente) 
+            arquivo.write(cliente)  # Formatar os dados do cliente e paciente em uma única string
 
             print(GREEN)
             print("Dados do Cliente e do Paciente cadastrados com sucesso!") # Exibir uma mensagem de sucesso
@@ -164,110 +165,108 @@ def alterar_dados():
                     
                     
                     novo_nome_paciente = input("Insira o novo nome do paciente: ")
-                    nova_raça = input("Insira a nova raça: ")
-                    nova_cor_da_pelagem = input("Insira a nova Cor da pelagem: ")
+                    novo_raça = input("Insira a nova raça: ")
+                    novo_cor_da_pelagem = input("Insira a nova Cor da pelagem: ")
                     novo_peso = float(input("Insira o novo peso : "))
-                    nova_data_de_nascimento= int(input("Insira o novo Cep: "))
-                    nova_restrição_alimentar = input("Insira alguma nova restrição alimentar: ")
-                    nova_medicação_continua= input("Insira alguma nova medicação de uso continua: ")
-                    nova_observações = input("Insira alguma nova observação: ")
+                    novo_data_de_nascimento= int(input("Insira a data de nascimento: "))
+                    novo_restrição_alimentar = input("Insira alguma nova restrição alimentar: ")
+                    novo_medicação_continua= input("Insira alguma nova medicação de uso continua: ")
+                    novo_observações = input("Insira alguma nova observação: ")
 
                     # Criar uma nova linha com os novos dados do aluno
-                    cliente = f"{novo_codigo_cliente},{novo_nome_tutor},{novo_contato},{novo_endereco},{novo_bairro},{novo_cep},{novo_ponto_de_referencia},{novo_nome_paciente},{nova_raça},{nova_cor_da_pelagem},{novo_peso},{nova_data_de_nascimento},{nova_restrição_alimentar},{nova_medicação_continua},{nova_observações}\n" # Formatar os dados do cl
+                    cliente = f"{novo_codigo_cliente},{novo_nome_tutor},{novo_contato},{novo_endereco},{novo_bairro},{novo_cep},{novo_ponto_de_referencia},{novo_nome_paciente},{novo_raça},{novo_cor_da_pelagem},{novo_peso},{novo_data_de_nascimento},{novo_restrição_alimentar},{novo_medicação_continua},{novo_observações}\n" # Formatar os dados do cl
 
                     print(GREEN)
                     print("Dados do aluno alterados com sucesso!")
                     print(RESET)
                     encontrado = True
                 
-                # Escrever a linha no arquivo
-                arquivo.write(linha)
+            arquivo.write(linha) # Escrever a linha no arquivo
         
-        # Verificar se nenhum aluno foi encontrado com o código fornecido
-        if not encontrado:
+        if not encontrado: # Verificar se nenhum aluno foi encontrado com o código fornecido
             print(RED)
             print("Nenhum aluno encontrado com o código fornecido.")
             print(RESET)
 
-    except ValueError:
-        # Capturar o erro caso algum valor inválido seja inserido pelo usuário
+    except ValueError: # Capturar o erro caso algum valor inválido seja inserido pelo usuário
         print(RED)
         print("Valor inválido. Certifique-se de digitar um valor numérico para o código do aluno e a idade, e um valor decimal para as notas.")
         print(RESET)
 
-    except FileNotFoundError:
-        # Capturar o erro caso o arquivo 'Dados.txt' não seja encontrado
+    except FileNotFoundError: # Capturar o erro caso o arquivo 'Dados.txt' não seja encontrado
         print(RED)
         print("Arquivo de dados não encontrado.")
         print(RESET)
 
-    except Exception as e:
-        # Capturar qualquer outro erro que possa ocorrer durante a alteração dos dados
+    except Exception as e: # Capturar qualquer outro erro que possa ocorrer durante a alteração dos dados
         print(RED)
         print("Ocorreu um erro ao alterar os dados:", str(e))
         print(RESET)
 
 def excluir_dados():
     try:
-        with open('Dados.txt', 'r') as arquivo:
-            linhas = arquivo.readlines()
+        with open('Dados.txt', 'r') as arquivo: # Abrir o arquivo 'Dados.txt' em modo de leitura ('r')
+            linhas = arquivo.readlines() # Ler todas as linhas do arquivo e armazená-las em uma lista
 
-        if not linhas:
+        if not linhas: # Verificar se não há linhas (dados) no arquivo
             print(RED)
             print("Não há nenhum dado cadastrado na Petshop Pequeninos.")
             print(RESET)
             return
 
-        codigo_cliente = int(input("Digite o código do cliente que deseja excluir: "))
+        codigo_cliente = int(input("Digite o código do cliente que deseja excluir: ")) # Solicitar ao usuário o código do cliente que deseja excluir
         encontrado = False
 
-        with open('Dados.txt', 'w') as arquivo:
-            for linha in linhas:
-                dados = linha.strip().split(',')
-                if int(dados[0]) == codigo_cliente:
-                    encontrado = True
+        with open('Dados.txt', 'w') as arquivo: # Abrir o arquivo 'Dados.txt' em modo de escrita ('w')
+            for linha in linhas: # Percorrer cada linha do arquivo
+                dados = linha.strip().split(',') # Dividir a linha em partes separadas por vírgula (',') e remover os espaços em branco
+                
+                
+                if int(dados[0]) == codigo_cliente: # Verificar se o código do cliente na linha atual corresponde ao código fornecido pelo usuário
+                    encontrado = True # Indicar que o cliente foi encontrado
                     print(GREEN)
-                    print("Dados do cliente excluídos com sucesso!")
+                    print("Dados do cliente excluídos com sucesso!") # Exibir mensagem de sucesso ao excluir os dados do cliente
                     print(RESET)
                 else:
-                    arquivo.write(linha)
+                    arquivo.write(linha) # Escrever a linha original no arquivo se o código não corresponder
 
-        if not encontrado:
+        if not encontrado: # Se nenhum cliente foi encontrado com o código fornecido
             print(RED)
             print("Nenhum cliente encontrado com o código fornecido.")
             print(RESET)
 
-    except ValueError:
+    except ValueError: # Capturar o erro se o usuário inserir um valor inválido
         print(RED)
         print("Valor inserido é inválido. Digite um valor numérico para o código do cliente.")
         print(RESET)
 
-    except FileNotFoundError:
+    except FileNotFoundError: # Capturar o erro se o arquivo 'Dados.txt' não for encontrado
         print(RED)
         print("Arquivo de dados não encontrado.")
         print(RESET)
 
-    except Exception as e:
+    except Exception as e: # Capturar qualquer outro erro que ocorra durante a exclusão dos dados
         print(RED)
         print("Ocorreu um erro ao excluir os dados:", str(e))
         print(RESET)
 
 def realizar_backup():
     try:
-        with open('Dados.txt', 'r') as arquivo_origem:
-            with open('backup_Dados.txt', 'w') as arquivo_backup:
-                conteudo = arquivo_origem.read()
-                arquivo_backup.write(conteudo)
+        with open('Dados.txt', 'r') as arquivo_origem: # Abrir o arquivo 'Dados.txt' em modo de leitura ('r')
+            with open('backup_Dados.txt', 'w') as arquivo_backup: # Abrir o arquivo 'backup_Dados.txt' em modo de escrita ('w')
+                conteudo = arquivo_origem.read() # Ler o conteúdo do arquivo de origem
+                arquivo_backup.write(conteudo) # Escrever o conteúdo lido no arquivo de backup
+    
                 print(GREEN)
-                print("Backup do arquivo realizado com sucesso!")
+                print("Backup do arquivo realizado com sucesso!") # Exibir mensagem de sucesso ao realizar o backup
                 print(RESET)
 
-    except FileNotFoundError:
+    except FileNotFoundError: # Capturar o erro se o arquivo 'Dados.txt' não for encontrado
         print(RED)
         print("Arquivo de dados não encontrado.")
         print(RESET)
 
-    except Exception as e:
+    except Exception as e: # Capturar qualquer outro erro que ocorra durante a realização do backup
         print(RED)
         print("Ocorreu um erro ao realizar o backup:", str(e))
         print(RESET)
